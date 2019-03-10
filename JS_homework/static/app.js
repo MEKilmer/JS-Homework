a018495b18213998781a95ab1ed2e9f54bf50a66
MEKilmer <43918978+MEKilmer@users.noreply.github.com>
2019-03-09 19:00:42 -0600

    Add files via upload

---
+++ JS_homework/static/app.js
@@ -0,0 +1,92 @@
+//from data.js
+var tableData = data;
+
+// Get references to the tbody element, input fields and buttons
+var $tbody = document.querySelector("tbody");
+var $dateInput = document.querySelector("#date");
+var $cityInput = document.querySelector("#city");
+var $stateInput = document.querySelector("#state");
+var $countryInput = document.querySelector("#country");
+var $shapeInput = document.querySelector("#shape");
+var $searchBtn = document.querySelector("#search");
+var $resetBtn = document.querySelector("#reset");
+
+
+// Add an event listener to the searchButton and resetButton, call functions when clicked
+$searchBtn.addEventListener("click", handleSearchButtonClick);
+$resetBtn.addEventListener("click", handleResetButtonClick);
+
+
+// renderTable renders the filtered data to the tbody
+function renderTable() {
+  $tbody.innerHTML = "";
+  for (var i = 0; i < tableData.length; i++) {
+    // Get get the current sighting object and its fields
+    var sighting = tableData[i];
+    var fields = Object.keys(sighting);
+    // Create a new row in the tbody, set the index to be i + startingIndex
+    var $row = $tbody.insertRow(i);
+    for (var j = 0; j < fields.length; j++) {
+      // For every field in the sighting object, create a new cell at set its inner text to be the current value at the current sighting's field
+      var field = fields[j];
+      var $cell = $row.insertCell(j);
+      $cell.innerText = sighting[field];
+    }
+  }
+}
+
+function handleSearchButtonClick() {
+  // go through search items with formatted user's search terms by removing leading and trailing whitespace
+
+  var filterDate = $dateInput.value.trim();
+  if (filterDate != "") {
+    tableData = data.filter(function (sighting) {
+      var sightingDate = sighting.datetime;
+      return sightingDate === filterDate;
+    });
+  };
+  var filterCity = $cityInput.value.trim().toLowerCase();
+  if (filterCity != "") {
+    tableData = tableData.filter(function (sighting) {
+      var sightingCity = sighting.city;
+      return sightingCity === filterCity;
+    });
+  };
+  var filterState = $stateInput.value.trim().toLowerCase();
+  if (filterState != "") {
+    tableData = tableData.filter(function (sighting) {
+      var sightingState = sighting.state;
+      return sightingState === filterState;
+    });
+  };
+  var filterCountry = $countryInput.value.trim().toLowerCase();
+  if (filterCountry != "") {
+    tableData = tableData.filter(function (sighting) {
+      var sightingCountry = sighting.country;
+      return sightingCountry === filterCountry;
+    });
+  };
+  var filterShape = $shapeInput.value.trim().toLowerCase();
+  if (filterShape != "") {
+    tableData = tableData.filter(function (sighting) {
+      var sightingShape = sighting.shape;
+      return sightingShape === filterShape;
+    });
+  };
+  renderTable();
+};
+
+
+// Reset the data and search form after a search
+function handleResetButtonClick() {
+  tableData = data;
+  $dateInput.value = "";
+  $cityInput.value = "";
+  $stateInput.value = "";
+  $countryInput.value = "";
+  $shapeInput.value = "";
+  renderTable();
+}
+
+// Render the table for the first time on page load
+renderTable();
